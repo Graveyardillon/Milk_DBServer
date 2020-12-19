f30c567e854e6d1ed554a74eb52abe531da77f75
maru713 <31789637+maru713@users.noreply.github.com>
2020-12-19 20:21:14 +0900

    bug fix delete loser

--- milk/lib/milk/tournaments.ex
+++ milk/lib/milk/tournaments.ex
@@ -551,9 +551,16 @@ defmodule Milk.Tournaments do
         [[_a,_b],[_c,_d]] -> Milk.Tournaments.delete_loser(x, loser)
         [_a,[_b,_c]] -> Milk.Tournaments.delete_loser(x, loser)
         [[_a,_b],_c] -> Milk.Tournaments.delete_loser(x, loser)
-        a when is_integer(a) -> a
+        a when is_integer(a) and is_list(loser) ->
+          Enum.find(loser, fn x -> x == a end)
+          |> case do
+            nil -> a
+            _ -> nil
+          end
+        a when a == loser -> nil
+        a when is_integer(a) and a != loser -> a
         _ ->
-          case x -- loser ++ [nil] do
+          case (x -- loser) do
             [a,b] -> [a,b]
             [a] -> a
             [] -> nil
