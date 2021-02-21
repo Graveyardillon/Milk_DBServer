9fafa17d19c93641ec89824b12589745c67d1330
Papillon6814 <so1rou.k@gmail.com>
2021-02-22 00:30:37 +0900

    fix: redis周りの設定記述追加

--- milk/lib/milk/application.ex
+++ milk/lib/milk/application.ex
@@ -20,6 +20,7 @@ defmodule Milk.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Milk.Supervisor]
+    Milk.TournamentProgress.flushall()
     Supervisor.start_link(children, opts)
   end
 
