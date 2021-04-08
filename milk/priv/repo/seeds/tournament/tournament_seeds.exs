13cb7169463b8b3d143886c46eb5ce1c7ff05d02
Papillon6814 <so1rou.k@gmail.com>
2021-04-08 21:56:19 +0900

    fix: seeder修正

--- milk/priv/repo/seeds/tournament/tournament_seeds.exs
+++ milk/priv/repo/seeds/tournament/tournament_seeds.exs
@@ -1,6 +1,7 @@
 use Timex
 
 alias Milk.Repo
+alias Milk.Tournaments
 alias Milk.Tournaments.Tournament
 alias Milk.Accounts.{
   Auth,
@@ -26,115 +27,121 @@ tomorrow =
   |> Timex.to_datetime()
 
 File.cp("./priv/repo/seeds/image/2pimp.jpg", "./static/image/tournament_thumbnail/2pimp.jpg")
-Repo.insert! %Tournament{
-  capacity: 4,
-  deadline: tomorrow,
-  description: "test tournament of size 4.",
-  event_date: tomorrow,
-  name: "test tournament size 4",
-  type: 1,
-  url: nil,
-  thumbnail_path: "2pimp",
-  password: nil,
-  game_name: "my awesome name",
-  start_recruiting: now,
-  start_notification_pid: nil,
-  master_id: user.id,
-  platform_id: 1,
-  game_id: nil
+attrs = %{
+  "capacity" => 4,
+  "deadline" => tomorrow,
+  "description" => "test tournament of size 4.",
+  "event_date" => tomorrow,
+  "name" => "test tournament size 4",
+  "type" => 1,
+  "url" => nil,
+  "thumbnail_path" => "2pimp",
+  "password" => nil,
+  "game_name" => "my awesome name",
+  "start_recruiting" => now,
+  "start_notification_pid" => nil,
+  "master_id" => user.id,
+  "platform_id" => 1,
+  "game_id" => nil
 }
+Tournaments.create_tournament(attrs, attrs["thumbnail_path"])
 
 File.cp("./priv/repo/seeds/image/damn.jpg", "./static/image/tournament_thumbnail/damn.jpg")
-Repo.insert! %Tournament{
-  capacity: 5,
-  deadline: tomorrow,
-  description: "test tournament of size 5.",
-  event_date: tomorrow,
-  name: "test tournament size 5",
-  type: 1,
-  url: nil,
-  thumbnail_path: "damn",
-  password: nil,
-  game_name: "my awesome name",
-  start_recruiting: now,
-  start_notification_pid: nil,
-  master_id: user.id,
-  platform_id: 1,
-  game_id: nil
+attrs = %{
+  "capacity" => 5,
+  "deadline" => tomorrow,
+  "description" => "test tournament of size 5.",
+  "event_date" => tomorrow,
+  "name" => "test tournament size 5",
+  "type" => 1,
+  "url" => nil,
+  "thumbnail_path" => "damn",
+  "password" => nil,
+  "game_name" => "my awesome name",
+  "start_recruiting" => now,
+  "start_notification_pid" => nil,
+  "master_id" => user.id,
+  "platform_id" => 1,
+  "game_id" => nil
 }
+Tournaments.create_tournament(attrs, attrs["thumbnail_path"])
 
 File.cp("./priv/repo/seeds/image/darksideofthemoon.jpg", "./static/image/tournament_thumbnail/darksideofthemoon.jpg")
-Repo.insert! %Tournament{
-  capacity: 6,
-  deadline: tomorrow,
-  description: "test tournament of size 6.",
-  event_date: tomorrow,
-  name: "test tournament size 6",
-  type: 1,
-  url: nil,
-  thumbnail_path: "darksideofthemoon",
-  password: nil,
-  game_name: "my awesome name",
-  start_recruiting: now,
-  start_notification_pid: nil,
-  master_id: user.id,
-  platform_id: 1,
-  game_id: nil
+attrs = %{
+  "capacity" => 6,
+  "deadline" => tomorrow,
+  "description" => "test tournament of size 6.",
+  "event_date" => tomorrow,
+  "name" => "test tournament size 6",
+  "type" => 1,
+  "url" => nil,
+  "thumbnail_path" => "darksideofthemoon",
+  "password" => nil,
+  "game_name" => "my awesome name",
+  "start_recruiting" => now,
+  "start_notification_pid" => nil,
+  "master_id" => user.id,
+  "platform_id" => 1,
+  "game_id" => nil
 }
+Tournaments.create_tournament(attrs, attrs["thumbnail_path"])
 
 File.cp("./priv/repo/seeds/image/kazemachi.jpg", "./static/image/tournament_thumbnail/kazemachi.jpg")
-Repo.insert! %Tournament{
-  capacity: 7,
-  deadline: tomorrow,
-  description: "test tournament of size 7.",
-  event_date: tomorrow,
-  name: "test tournament size 7",
-  type: 1,
-  url: nil,
-  thumbnail_path: "kazemachi",
-  password: nil,
-  game_name: "my awesome name",
-  start_recruiting: now,
-  start_notification_pid: nil,
-  master_id: user.id,
-  platform_id: 1,
-  game_id: nil
+attrs = %{
+  "capacity" => 7,
+  "deadline" => tomorrow,
+  "description" => "test tournament of size 7.",
+  "event_date" => tomorrow,
+  "name" => "test tournament size 7",
+  "type" => 1,
+  "url" => nil,
+  "thumbnail_path" => "kazemachi",
+  "password" => nil,
+  "game_name" => "my awesome name",
+  "start_recruiting" => now,
+  "start_notification_pid" => nil,
+  "master_id" => user.id,
+  "platform_id" => 1,
+  "game_id" => nil
 }
+Tournaments.create_tournament(attrs, attrs["thumbnail_path"])
 
 File.cp("./priv/repo/seeds/image/nevermind.jpg", "./static/image/tournament_thumbnail/nevermind.jpg")
-Repo.insert! %Tournament{
-  capacity: 8,
-  deadline: tomorrow,
-  description: "test tournament of size 8.",
-  event_date: tomorrow,
-  name: "test tournament size 8",
-  type: 1,
-  url: nil,
-  thumbnail_path: "nevermind",
-  password: nil,
-  game_name: "my awesome name",
-  start_recruiting: now,
-  start_notification_pid: nil,
-  master_id: user.id,
-  platform_id: 1,
-  game_id: nil
+attrs = %{
+  "capacity" => 8,
+  "deadline" => tomorrow,
+  "description" => "test tournament of size 8.",
+  "event_date" => tomorrow,
+  "name" => "test tournament size 8",
+  "type" => 1,
+  "url" => nil,
+  "thumbnail_path" => "nevermind",
+  "password" => nil,
+  "game_name" => "my awesome name",
+  "start_recruiting" => now,
+  "start_notification_pid" => nil,
+  "master_id" => user.id,
+  "platform_id" => 1,
+  "game_id" => nil
 }
+Tournaments.create_tournament(attrs, attrs["thumbnail_path"])
 
 File.cp("./priv/repo/seeds/image/solid_state_survivor.jpg", "./static/image/tournament_thumbnail/solid_state_survivor.jpg")
-Repo.insert! %Tournament{
-  capacity: 32,
-  deadline: tomorrow,
-  description: "test tournament of size 32.",
-  event_date: tomorrow,
-  name: "test tournament size 32",
-  type: 1,
-  url: nil,
-  thumbnail_path: "solid_state_survivor",
-  password: nil,
-  game_name: "my awesome name",
-  start_recruiting: now,
-  start_notification_pid: nil,
-  master_id: user.id,
-  platform_id: 1,
-  game_id: nil
+attrs = %{
+  "capacity" => 32,
+  "deadline" => tomorrow,
+  "description" => "test tournament of size 32.",
+  "event_date" => tomorrow,
+  "name" => "test tournament size 32",
+  "type" => 1,
+  "url" => nil,
+  "thumbnail_path" => "solid_state_survivor",
+  "password" => nil,
+  "game_name" => "my awesome name",
+  "start_recruiting" => now,
+  "start_notification_pid" => nil,
+  "master_id" => user.id,
+  "platform_id" => 1,
+  "game_id" => nil
 }
+Tournaments.create_tournament(attrs, attrs["thumbnail_path"])
