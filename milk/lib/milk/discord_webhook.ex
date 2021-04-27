af6a64c2ff3abba959fb3c6783ee4b5ae8fa6539
Papillon6814 <so1rou.k@gmail.com>
2021-04-28 02:58:35 +0900

    feat: post_text関数の作成

--- milk/lib/milk/discord_webhook.ex
+++ milk/lib/milk/discord_webhook.ex
@@ -8,5 +8,11 @@ defmodule Milk.DiscordWebhook do
   """
   def post_text(text) do
     url = "https://discord.com/api/webhooks/836661276460449862/RjC4AIzBHQiOc3QbXBkAuuMpumEccWxfecbQ545GIH4Y0XJYoux2D1H11_UxMP2CXCSO"
+
+    HTTPoison.post(
+      url,
+      Jason.encode!(%{content: text}),
+      "Content-Type": "application/json"
+    )
   end
 end
