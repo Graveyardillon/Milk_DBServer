7d4162276f2280f3af1bf0d1b73028691e0d301e
Papillon6814 <so1rou.k@gmail.com>
2021-04-28 02:55:56 +0900

    feat: webhook用モジュール作成

---
+++ milk/lib/milk/discord_webhook.ex
@@ -0,0 +1,12 @@
+defmodule Milk.DiscordWebhook do
+  @moduledoc """
+  Discord webhook.
+  """
+
+  @doc """
+  Send a webhook notification.
+  """
+  def post_text(text) do
+    url = "https://discord.com/api/webhooks/836661276460449862/RjC4AIzBHQiOc3QbXBkAuuMpumEccWxfecbQ545GIH4Y0XJYoux2D1H11_UxMP2CXCSO"
+  end
+end
