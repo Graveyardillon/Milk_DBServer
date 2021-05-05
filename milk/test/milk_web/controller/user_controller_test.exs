7d5dd5d606529d4faded97525603a69efa27842e
Papillon6814 <so1rou.k@gmail.com>
2021-05-06 01:20:27 +0900

    test: ユーザー削除のテスト

--- milk/test/milk_web/controller/user_controller_test.exs
+++ milk/test/milk_web/controller/user_controller_test.exs
@@ -188,6 +188,35 @@ defmodule MilkWeb.UserControllerTest do
       conn = delete(conn, Routes.user_path(conn, :delete), %{id: user["id"], password: @create_attrs["password"], email: user["email"], token: token})
       assert json_response(conn, 200)["result"]
     end
+
+    test "does not work (invalid password)", %{conn: conn} do
+      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
+      response = json_response(conn, 200)
+      token = response["token"]
+      user = response["data"]
+
+      conn = delete(conn, Routes.user_path(conn, :delete), %{id: user["id"], password: "wrong_passw0rD", email: user["email"], token: token})
+      refute json_response(conn, 200)["result"]
+    end
+
+    test "does not work (invalid token)", %{conn: conn} do
+      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
+      response = json_response(conn, 200)
+      user = response["data"]
+
+      conn = delete(conn, Routes.user_path(conn, :delete), %{id: user["id"], password: @create_attrs["password"], email: user["email"], token: "toooken"})
+      refute json_response(conn, 200)["result"]
+    end
+
+    test "does not work (invalid email)", %{conn: conn} do
+      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
+      response = json_response(conn, 200)
+      token = response["token"]
+      user = response["data"]
+
+      conn = delete(conn, Routes.user_path(conn, :delete), %{id: user["id"], password: @create_attrs["password"], email: "wrong_email@wrong.wng", token: token})
+      refute json_response(conn, 200)["result"]
+    end
   end
 
   describe "change password" do
