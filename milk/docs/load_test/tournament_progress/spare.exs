7e3b11410c07f2d70e4221d6779d165c370cf3da
Papillon6814 <so1rou.k@gmail.com>
2021-05-30 15:33:52 +0900

    load_test: 主催まで

--- milk/docs/load_test/tournament_progress/spare.exs
+++ milk/docs/load_test/tournament_progress/spare.exs
@@ -1,5 +1,59 @@
+use Timex
+
 url = "http://localhost:4000/api/load_test/start"
 HTTPoison.post(url, Jason.encode!(%{}), "Content-Type": "application/json")
+
+Process.sleep(1000)
+
+1..25
+|> Enum.to_list()
+|> Enum.map(fn n ->
+  attr = %{"email" => to_string(n)<>"@mail.co.m", "password" => "Password123", "name" => to_string(n)<>"user123"}
+  {:ok, %HTTPoison.Response{body: body}} =
+    HTTPoison.post(
+      "http://localhost:4000/api/user/signup",
+      Jason.encode!(%{"user" => attr}),
+      "Content-Type": "application/json"
+    )
+  {:ok, map} = Poison.decode(body)
+  map
+end)
+|> Enum.map(fn response ->
+  now = Timex.now()
+  tomorrow = Timex.now()
+    |> Timex.add(Timex.Duration.from_days(1))
+    |> Timex.to_datetime()
+
+  master_id = response["data"]["id"]
+  attrs = %{
+    "master_id" => master_id,
+    "capacity" => 4,
+    "deadline" => tomorrow,
+    "description" => "awesome tournament",
+    "event_date" => tomorrow,
+    "name" => "test tournament size 4",
+    "type" => 2,
+    "url" => nil,
+    "thumbnail_path" => nil,
+    "password" => nil,
+    "game_name" => "my awesome name",
+    "start_recruiting" => now,
+    "platform" => 1,
+    "game_id" => nil,
+    "join" => false
+  }
+  |> Poison.encode!()
+  form = [{"file", ""}, {"tournament", attrs}]
+  {:ok, %HTTPoison.Response{body: body}} =
+    HTTPoison.post(
+      "http://localhost:4000/api/tournament",
+      {:multipart, form},
+      [{"Content-Type", "multipart/form-data"}],
+      ssl: [{:versions, [:'tlsv1.2']}]
+    )
+  {:ok, map} = Poison.decode(body)
+end)
+
 
 Process.sleep(3000)
 
