7bd3f7b22f446d7749011759ed1fb9cd97631fea
Papillon6814 <so1rou.k@gmail.com>
2021-06-27 18:45:51 +0900

    feat: チャットのseederに改行のものを追加

--- milk/priv/repo/seeds/chat/chat_seed_on_tournament1.exs
+++ milk/priv/repo/seeds/chat/chat_seed_on_tournament1.exs
@@ -8,3 +8,12 @@ end)
 |> Enum.each(fn map ->
   Chat.dialogue(map)
 end)
+
+1..20
+|> Enum.to_list()
+|> Enum.map(fn n ->
+  %{"user_id" => rem(n, 4)+1, "chat_room_id" => rem(n, 3)+1, "word" => "Let's break line\n (broken): #{n}"}
+end)
+|> Enum.each(fn map ->
+  Chat.dialogue(map)
+end)
