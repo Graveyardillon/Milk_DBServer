4f9cdee0c8a66a601a842a1352adc837fa852370
Papillon6814 <so1rou.k@gmail.com>
2021-06-27 16:13:18 +0900

    feat: chatのseeder作成

---
+++ milk/priv/repo/seeds/chat/chat_seed_on_tournament1.exs
@@ -0,0 +1,10 @@
+alias Milk.Chat
+
+1..20
+|> Enum.to_list()
+|> Enum.map(fn n ->
+  %{"user_id" => rem(n, 4)+1, "chat_room_id" => rem(n, 3)+1, "word" => "Hello #{n}!"}
+end)
+|> Enum.each(fn map ->
+  Chat.dialogue(map)
+end)
