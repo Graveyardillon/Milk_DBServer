3c2869e527428f3b494d274012f235f3e4ad7d89
Papillon6814 <so1rou.k@gmail.com>
2021-07-01 19:53:37 +0900

    feat: icon_pathの列を追加

--- milk/lib/milk/notif/notification.ex
+++ milk/lib/milk/notif/notification.ex
@@ -10,6 +10,7 @@ defmodule Milk.Notif.Notification do
     field :process_code, :integer
     field :data, :string
     field :is_checked, :boolean, default: false
+    field :icon_path, :string
 
     belongs_to :user, User
 
@@ -19,13 +20,13 @@ defmodule Milk.Notif.Notification do
   @doc false
   def changeset(notification, attrs) do
     notification
-    |> cast(attrs, [:content, :process_code, :data, :is_checked])
+    |> cast(attrs, [:content, :process_code, :data, :is_checked, :icon_path])
     |> validate_required([:content])
   end
 
   @doc false
   def update_changeset(notification, attrs) do
     notification
-    |> cast(attrs, [:content, :process_code, :data, :is_checked])
+    |> cast(attrs, [:content, :process_code, :data, :is_checked, :icon_path])
   end
 end
