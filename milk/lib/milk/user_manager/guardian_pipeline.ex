637ad37f2d6abe8912fdda9f25e60421c0ded986
Papillon6814 <so1rou.k@gmail.com>
2021-07-28 02:40:36 +0900

    feat: チャット用

--- milk/lib/milk/user_manager/guardian_pipeline.ex
+++ milk/lib/milk/user_manager/guardian_pipeline.ex
@@ -39,7 +39,9 @@ defmodule Milk.UserManager.GuardianPipeline do
       String.contains?(conn.request_path, "api/user/login") or
       String.contains?(conn.request_path, "api/user/signup") or
       String.contains?(conn.request_path, "api/user/signin") or
-      String.contains?(conn.method, "GET")
+      String.contains?(conn.method, "GET") or
+      String.contains?(conn.request_path, "api/chat/create_dialogue") or
+      String.contains?(conn.request_path, "api/notification/create")
     ) do
       conn
     else
