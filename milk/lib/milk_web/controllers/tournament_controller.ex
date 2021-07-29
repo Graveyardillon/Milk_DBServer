48750be31d23ab72b4a472320682201b369053c4
Papillon6814 <so1rou.k@gmail.com>
2021-07-29 21:37:44 +0900

    feat: is_finishedの修正

--- milk/lib/milk_web/controllers/tournament_controller.ex
+++ milk/lib/milk_web/controllers/tournament_controller.ex
@@ -1070,8 +1070,8 @@ defmodule MilkWeb.TournamentController do
             TournamentProgress.delete_match_pending_list(opponent_id, tournament_id)
             TournamentProgress.delete_score(tournament_id, user_id)
             TournamentProgress.delete_score(tournament_id, opponent_id)
-            finish_as_needed(tournament_id, opponent_id)
-            json(conn, %{validated: true, completed: true})
+            is_finished = finish_as_needed?(tournament_id, opponent_id)
+            json(conn, %{validated: true, completed: true, is_finished: is_finished})
 
           n < score ->
             Tournaments.delete_loser_process(tournament_id, [opponent_id])
@@ -1080,19 +1080,19 @@ defmodule MilkWeb.TournamentController do
             TournamentProgress.delete_match_pending_list(opponent_id, tournament_id)
             TournamentProgress.delete_score(tournament_id, user_id)
             TournamentProgress.delete_score(tournament_id, opponent_id)
-            finish_as_needed(tournament_id, user_id)
-            json(conn, %{validated: true, completed: true})
+            is_finished = finish_as_needed?(tournament_id, user_id)
+            json(conn, %{validated: true, completed: true, is_finished: is_finished})
 
           true ->
-            json(conn, %{validated: false, completed: false})
+            json(conn, %{validated: false, completed: false, is_finished: false})
         end
 
       [] ->
-        json(conn, %{validated: true, completed: false})
+        json(conn, %{validated: true, completed: false, is_finished: false})
     end
   end
 
-  defp finish_as_needed(tournament_id, winner_id) do
+  defp finish_as_needed?(tournament_id, winner_id) do
     match_list = TournamentProgress.get_match_list(tournament_id)
 
     if is_integer(match_list) do
@@ -1112,10 +1112,9 @@ defmodule MilkWeb.TournamentController do
       TournamentProgress.delete_fight_result_of_tournament(tournament_id)
       TournamentProgress.delete_duplicate_users_all(tournament_id)
       TournamentProgress.delete_lose_processes(tournament_id)
-    end
-
-    if match_list == [] do
-      Logger.error("Match list error on finish as needed")
+      true
+    else
+      false
     end
   end
 
@@ -1138,7 +1137,7 @@ defmodule MilkWeb.TournamentController do
         Tournaments.promote_rank(%{"tournament_id" => tournament_id, "user_id" => winner["id"]})
         Tournaments.score(tournament_id, winner["id"], target_user_id, 0, -1, 0)
         Tournaments.delete_loser_process(tournament_id, [target_user_id])
-        finish_as_needed(tournament_id, winner["id"])
+        finish_as_needed?(tournament_id, winner["id"])
 
       {:wait, nil} ->
         tournament_id
