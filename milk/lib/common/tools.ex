215690c3dee1f206ce102eb06450a0bc6f39395e
Papillon6814 <so1rou.k@gmail.com>
2021-10-27 11:14:26 +0900

    refactor: tools

--- milk/lib/common/tools.ex
+++ milk/lib/common/tools.ex
@@ -21,6 +21,7 @@ defmodule Common.Tools do
   @doc """
   Create error message
   """
+  @spec create_error_message(any()) :: String.t()
   def create_error_message(error) when is_binary(error), do: error
   def create_error_message(error) do
     Enum.reduce(error, "", fn {key, value}, acc ->
@@ -54,23 +55,28 @@ defmodule Common.Tools do
   @doc """
   Turn into tuple {:ok, data}
   """
+  @spec into_ok_tuple(any()) :: {:ok, any()}
   def into_ok_tuple(data), do: {:ok, data}
 
   @doc """
   Turn into tuple {:error, data}
   """
+  @spec into_error_tuple(any()) :: {:error, any()}
   def into_error_tuple(data), do: {:error, data}
 
   @doc """
   Checks if all map elements are nil
   """
+  @spec is_all_map_elements_nil?(map()) :: boolean()
+  def is_all_map_elements_nil?(data) when not is_map(data), do: false
   def is_all_map_elements_nil?(map) do
-    unless is_map(map), do: raise("Argument is not map")
-
     map
     |> Enum.filter(fn {_k, v} -> !is_nil(v) end)
-    |> (fn x -> x == [] end).()
+    |> Enum.empty?()
   end
+
+  @spec is_power_of_two?(integer()) :: boolean()
+  def is_power_of_two?(num), do: num != 0 and num && num - 1 === 0
 
   @doc """
   String to json map if possible.
