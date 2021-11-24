88d39366fcb0b5b5efb328d2881d33e1344f26e7
Papillon6814 <so1rou.k@gmail.com>
2021-11-24 13:16:17 +0900

    feat: get_tournamentのbench記述

--- milk/bench/milk/tournaments_bench.exs
+++ milk/bench/milk/tournaments_bench.exs
@@ -1,3 +1,24 @@
 defmodule Milk.TournamentsBench do
+  @moduledoc """
+  Tournamets.exの性能テスト
+  """
+  use Benchfella
+  use Common.Fixtures
 
+  alias Milk.Tournaments
+
+  @tournament_list Enum.to_list(1..128)
+
+  setup_all       do: Application.ensure_all_started(:milk)
+  teardown_all _, do: Application.stop(:milk)
+
+  bench "get_tournament", [t: tournaments] do
+    t
+    |> Map.get(&Map.get(&1, :id))
+    |> Enum.each(&Tournaments.get_tournament(&1))
+  end
+
+  defp tournaments() do
+    Enum.map(@tournament_list, &fixture_tournament(num: &1))
+  end
 end
