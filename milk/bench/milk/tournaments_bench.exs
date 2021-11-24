6c2e24d49d343e102454b152e92ed528fcc5b348
Papillon6814 <so1rou.k@gmail.com>
2021-11-24 15:27:31 +0900

    feat: bench

--- milk/bench/milk/tournaments_bench.exs
+++ milk/bench/milk/tournaments_bench.exs
@@ -9,16 +9,24 @@ defmodule Milk.TournamentsBench do
 
   @tournament_list Enum.to_list(1..128)
 
-  setup_all       do: Application.ensure_all_started(:milk)
+  setup_all       do
+    result = Application.ensure_all_started(:milk)
+    fixture_user()
+    result
+  end
   teardown_all _, do: Application.stop(:milk)
 
   bench "get_tournament", [t: tournaments] do
-    t
-    |> Map.get(&Map.get(&1, :id))
-    |> Enum.each(&Tournaments.get_tournament(&1))
+    Enum.each(t, &Tournaments.get_tournament(&1))
+  end
+
+  bench "load_tournament",[t: tournaments] do
+    Enum.each(t, &Tournaments.load_tournament(&1))
   end
 
   defp tournaments() do
-    Enum.map(@tournament_list, &fixture_tournament(num: &1))
+    @tournament_list
+    |> Enum.map(&fixture_tournament(num: &1, master_id: 1))
+    |> Enum.map(&(&1.id))
   end
 end
