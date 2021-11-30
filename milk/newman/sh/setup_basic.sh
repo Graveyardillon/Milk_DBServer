73ee6f1affcb9902db096cb5cbe89c0622bdc71c
Papillon6814 <so1rou.k@gmail.com>
2021-12-01 01:30:45 +0900

    fix: シェルスクリプトの警告解消

--- milk/newman/sh/setup_basic.sh
+++ milk/newman/sh/setup_basic.sh
@@ -4,7 +4,7 @@ ts-node ../src/user.ts
 ts-node ../src/users.ts --count 10
 ts-node ../src/basic/basic.ts
 
-if [ $1 == '--master_as_entrant' ]; then
+if [ "$1" == '--master_as_entrant' ]; then
     ts-node ../src/basic/fill.ts --master_as_entrant
 else
     ts-node ../src/basic/fill.ts
--- milk/newman/sh/setup_flipban.sh
+++ milk/newman/sh/setup_flipban.sh
@@ -4,7 +4,7 @@ ts-node ../src/user.ts
 ts-node ../src/users.ts --count 50
 ts-node ../src/flipban/flipban.ts
 
-if [ $1 == '--master_as_entrant' ]; then
+if [ "$1" == '--master_as_entrant' ]; then
     ts-node ../src/flipban/fill.ts --master_as_entrant
 else
     ts-node ../src/flipban/fill.ts
