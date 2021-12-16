09f163efa3162fcfbc404d94cc63ad5d8d21616b
Papillon6814 <so1rou.k@gmail.com>
2021-12-16 17:50:44 +0900

    feat: platformsのところの処理を改善

--- milk/lib/milk/platforms.ex
+++ milk/lib/milk/platforms.ex
@@ -3,21 +3,22 @@ defmodule Milk.Platforms do
   The platforms context.
   """
 
-  import Common.Sperm
   import Ecto.Query, warn: false
 
   alias Milk.Platforms.Platform
   alias Milk.Repo
 
   def create_basic_platforms() do
+    platform_names = [
+      "pc", "mobile", "the other"
+    ]
+
     Platform
-    |> where([p], p.name == "pc" or p.name == "mobile")
+    |> where([p], p.name in ^platform_names)
     |> Repo.all()
     |> Enum.map(&(&1.name))
-    ~> platform_names
-
-    unless "pc" in platform_names,     do: create_platform(%{"name" => "pc"})
-    unless "mobile" in platform_names, do: create_platform(%{"name" => "mobile"})
+    |> Enum.reject(&(&1 in platform_names))
+    |> Enum.each(&create_platform(%{"name" => &1}))
   end
 
   def create_platform(attrs \\ %{}) do
