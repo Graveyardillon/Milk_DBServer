2701d8656e16032e647711bc29695e7de1aa55fb
Papillon6814 <so1rou.k@gmail.com>
2021-12-19 14:55:25 +0900

    refator: dialyzer-ignoreに追加の記述を入れてみる

--- milk/.dialyzer_ignore.exs
+++ milk/.dialyzer_ignore.exs
@@ -2,5 +2,6 @@
   {"lib/milk/tournaments.ex", :pattern_match, 1},
   {"test/support/channel_case.ex", :unknown_function},
   {"test/support/conn_case.ex", :unknown_function},
-  {"test/support/data_case.ex", :unknown_function}
+  {"test/support/data_case.ex", :unknown_function},
+  {"lib/milk_web/controllers/tournament_controller", :has_no_local_return}
 ]
