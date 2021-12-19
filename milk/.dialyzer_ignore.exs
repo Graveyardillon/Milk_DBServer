db250d282ea6bdf9baa59dd483cbdf19cbccdab8
Papillon6814 <so1rou.k@gmail.com>
2021-12-19 19:31:42 +0900

    Update .dialyzer_ignore.exs

--- milk/.dialyzer_ignore.exs
+++ milk/.dialyzer_ignore.exs
@@ -3,5 +3,5 @@
   {"test/support/channel_case.ex", :unknown_function},
   {"test/support/conn_case.ex", :unknown_function},
   {"test/support/data_case.ex", :unknown_function},
-  {"lib/milk_web/controllers/tournament_controller", :_no_return}
+  {"lib/milk_web/controllers/tournament_controller", :no_return}
 ]
