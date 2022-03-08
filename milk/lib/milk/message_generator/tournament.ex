59dd333b72af88a7bad42def70d1387a2fd0ebd9
Papillon6814 <so1rou.k@gmail.com>
2022-03-08 22:40:41 +0900

    feat: multilang module

--- milk/lib/milk/message_generator/tournament.ex
+++ milk/lib/milk/message_generator/tournament.ex
@@ -2,7 +2,26 @@ defmodule Milk.MessageGenerator.Tournament do
   @moduledoc """
   多言語のメッセージを扱うためのやつ
   """
-  def got_invitation_from(user_name, "japanese"),   do: "#{user_name}からチーム招待されました。"
-  def got_invitation_from(user_name, "english"),    do: "Received an invitation from #{user_name}."
-  def got_invitation_from(user_name, "indonesian"), do: "Menerima undangan dari #{user_name}."
+
+  @default_lang "english"
+
+  def got_invitation_from(name, "japanese"),   do: "#{name}からチーム招待されました。"
+  def got_invitation_from(name, "english"),    do: "Received an invitation from #{name}."
+  def got_invitation_from(name, "indonesian"), do: "Menerima undangan dari #{name}."
+  def got_invitation_from(name, _),            do: __MODULE__.got_invitation_from(name, @default_lang)
+
+  def joined_team(name, "japanese"),   do: "#{name}がチームに参加しました。"
+  def joined_team(name, "english"),    do: "#{name} joined your team."
+  def joined_team(name, "indonesian"), do: "#{name} bergabung dengan tim Anda."
+  def joined_team(name, _),            do: __MODULE__.joined_team(name, @default_lang)
+
+  def reject_team_invitation(name, "japanese"),   do: "#{name}がチームへの招待を辞退しました。"
+  def reject_team_invitation(name, "english"),    do: "#{name} rejected your team invitation."
+  def reject_team_invitation(name, "indonesian"), do: "#{name} menolak undangan timmu."
+  def reject_team_invitation(name, _),            do: __MODULE__.reject_team_invitation(name, @default_lang)
+
+  def received_discord_server_invitation_of(name, "japanese"),   do: "#{name}のDiscordサーバーへの招待を受け取りました。"
+  def received_discord_server_invitation_of(name, "english"),    do: "Received an invitation of #{name}'s discord server."
+  def received_discord_server_invitation_of(name, "indonesian"), do: "Menerima undangan dari server perselisihan #{name}. "
+  def received_discord_server_invitation_of(name, _),            do: __MODULE__.received_discord_server_invitation_of(name, @default_lang)
 end
--- milk/lib/milk/tournaments.ex
+++ milk/lib/milk/tournaments.ex
@@ -47,6 +47,8 @@ defmodule Milk.Tournaments do
     TournamentLog
   }
 
+  alias Milk.MessageGenerator.Tournament, as: TournamentMessageGenerator
+
   alias Milk.Notif.Notification
 
   alias Milk.Tournaments.{
@@ -3790,7 +3792,7 @@ defmodule Milk.Tournaments do
     |> Map.get(:user)
     ~> leader
 
-    title_str = "#{leader.name} からチーム招待されました"
+    title_str = TournamentMessageGenerator.got_invitation_from(leader.name, leader.language)
 
     Notification
     |> where([n], n.title == ^title_str)
@@ -4206,7 +4208,7 @@ defmodule Milk.Tournaments do
       "user_id" => invitation.team_member.user_id,
       "process_id" => "TEAM_INVITE",
       "icon_path" => invitation.sender.icon_path,
-      "title" => "#{invitation.sender.name} からチーム招待されました",
+      "title" => TournamentMessageGenerator.got_invitation_from(invitation.sender.name, invitation.sender.language),
       "body_text" => "",
       "data" =>
         Jason.encode!(%{
@@ -4227,7 +4229,7 @@ defmodule Milk.Tournaments do
         device_token: device.token,
         process_id: "TEAM_INVITE",
         title: "",
-        message: "#{invitation.sender.name} からチーム招待されました",
+        message: TournamentMessageGenerator.got_invitation_from(invitation.sender.name, invitation.sender.language),
         params: %{"invitation_id" => invitation.id}
       }
       |> Milk.Notif.push_ios()
@@ -4259,7 +4261,8 @@ defmodule Milk.Tournaments do
         |> case do
           %TeamInvitation{} = invitation ->
             create_team_invitation_result_notification(invitation, true)
-            verify_team_as_needed(team_member.team_id)
+            team_member.team_id
+            |> verify_team_as_needed()
             |> case do
               {:ok, _}                                      -> {:ok, team_member}
               {:error, "short of confirmed" <> _ = message} -> {:error, message, team_member}
@@ -4283,7 +4286,7 @@ defmodule Milk.Tournaments do
           "user_id" => invitation.sender.id,
           "process_id" => "TEAM_INVITE_RESULT",
           "icon_path" => invitation.team_member.user.icon_path,
-          "title" => "#{invitation.team_member.user.name} がチームに参加しました",
+          "title" => TournamentMessageGenerator.joined_team(invitation.team_member.user.name, invitation.team_member.user.language),
           "body_text" => "",
           "data" =>
             Jason.encode!(%{
@@ -4298,7 +4301,7 @@ defmodule Milk.Tournaments do
             device_token: device.token,
             process_id: "TEAM_INVITE_RESULT",
             title: "",
-            message: "#{invitation.team_member.user.name} がチームに参加しました",
+            message: TournamentMessageGenerator.joined_team(invitation.team_member.user.name, invitation.team_member.language),
             params: %{"invitation_id" => invitation.id}
           }
           |> Milk.Notif.push_ios()
@@ -4309,7 +4312,7 @@ defmodule Milk.Tournaments do
           "user_id" => invitation.sender.id,
           "process_id" => "TEAM_INVITE_RESULT",
           "icon_path" => invitation.team_member.user.icon_path,
-          "title" => "#{invitation.team_member.user.name} がチームへの招待を辞退しました",
+          "title" => TournamentMessageGenerator.reject_team_invitation(invitation.team_member.user.name, invitation.team_member.user.language),
           "body_text" => "",
           "data" =>
             Jason.encode!(%{
@@ -4324,7 +4327,7 @@ defmodule Milk.Tournaments do
             device_token: device.token,
             process_id: "TEAM_INVITE_RESULT",
             title: "",
-            message: "#{invitation.team_member.user.name} がチームへの招待を辞退しました",
+            message: TournamentMessageGenerator.reject_team_invitation(invitation.team_member.user.name, invitation.team_member.user.language),
             params: %{"invitation_id" => invitation.id}
           }
           |> Milk.Notif.push_ios()
@@ -4425,7 +4428,7 @@ defmodule Milk.Tournaments do
         "user_id" => member.user_id,
         "process_id" => "DISCORD_SERVER_INVITATION",
         "icon_path" => tournament.thumbnail_path,
-        "title" => "#{tournament.name}のDiscordサーバーへの招待を受け取りました",
+        "title" => TournamentMessageGenerator.received_discord_server_invitation_of(member.user.name, member.user.language),
         "body_text" => "",
         "data" => Jason.encode!(%{url: url})
       }
