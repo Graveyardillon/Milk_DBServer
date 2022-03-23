72beba7836fa1b15c91553fab5fa4b58f1abd007
Papillon6814 <so1rou.k@gmail.com>
2022-03-23 20:37:36 +0900

    chore: basic ts

--- milk/newman/sh/setup_basic.sh
+++ milk/newman/sh/setup_basic.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 ts-node ../src/user.ts
-ts-node ../src/users.ts --count 10
-ts-node ../src/basic/basic.ts
+
+if [ "$1" == '--team' ]; then
+    ts-node ../src/users.ts --count 50
+    ts-node ../src/basic/basic.ts --is_team true --team_size 5
+else
+    ts-node ../src/users.ts --count 10
+    ts-node ../src/basic/basic.ts
+fi
 
 if [ "$1" == '--master_as_entrant' ]; then
     ts-node ../src/basic/fill.ts --master_as_entrant
+elif [ "$1" == '--team' ]; then
+    ts-node ../src/basic/fill_team.ts
 else
     ts-node ../src/basic/fill.ts
 fi
