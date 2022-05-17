7fa0fb37c70472fe5fc75653dd26fbe7e15e397e
Papillon6814 <so1rou.k@gmail.com>
2022-05-17 12:29:31 +0900

    test: undo_start

--- milk/test/milk_web/controller/bracket_controller_test.exs
+++ milk/test/milk_web/controller/bracket_controller_test.exs
@@ -217,4 +217,25 @@ defmodule MilkWeb.BracketControllerTest do
       assert bracket.is_started
     end
   end
+
+  describe "undo start" do
+    test "works", %{conn: conn} do
+      bracket = fixture_bracket()
+
+      names = [
+        "test1user",
+        "test2user",
+        "test3user",
+        "test4user"
+      ]
+      conn = post(conn, Routes.bracket_path(conn, :create_participants), %{"names" => names, "bracket_id" => bracket.id})
+
+      conn = post(conn, Routes.bracket_path(conn, :start), bracket_id: bracket.id)
+      conn = post(conn, Routes.bracket_path(conn, :undo_start), bracket_id: bracket.id)
+
+      bracket = Brackets.get_bracket(bracket.id)
+
+      refute bracket.is_started
+    end
+  end
 end
